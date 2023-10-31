b10fe061b901daf2d573ed8407009151a746df90
lucasizi <lucasizi10@hotmail.com>
2023-10-31 10:49:26 -0300

    att

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,24 +1,11 @@
-# list comprehension
+import json
+import urllib
+import requests
 
-x = [1, 2, 3, 4, 5]
-y = []
-
-for i in x:
-    y.append(i**2)
-
-print(x)
-print(y)
-
-a = [1, 2, 3, 4, 5]
-b = [i**2 for i in a]
-
-print(x)
-print(y)
-
-a = [1, 2, 3, 4, 5]
-b = [i**2 for i in a]
-c = [i for i in a if i%2==1]
-
-print(x)
-print(y)
-print(c)
+url = 'https://parseapi.back4app.com/classes/Cepcorreios_CEP?limit=10'
+headers = {
+    'X-Parse-Application-Id': 'KDscoJcVWclrKOKMoSmmkwzVnr2NVTJleUA17X0f', # This is your app's application id
+    'X-Parse-REST-API-Key': 'qkxoXIfgK4oM3NAqdc0lFUw73wVpkHXymNdwHdoW' # This is your app's REST API key
+}
+data = json.loads(requests.get(url, headers=headers).content.decode('utf-8')) # Here you have the data that you need
+print(json.dumps(data, indent=2))
--- matematicas.py
+++ matematicas.py
@@ -1,37 +1,11 @@
-class Message:(object)
-    def __init__(self, message: str, sender: int, receiver: int) -> None:
-        self.message = message
-        self.sender = sender
-        self.receiver = receiver
+import json
+import urllib
+import requests
 
-    def __str__(self) -> str:
-        return self.message
-
-    def __eq__(self, other: object) -> bool:
-        if isinstance(other, Message):
-            return self.message == other.message
-        return False
-
-
-def process_queries(queries, messages):
-    for query in queries:
-        instruction, *data = query.split()
-        if instruction == 'print':
-            index = int(data[0])
-            print(messages[index])
-        elif instruction == 'eq':
-            i1, i2 = map(int, data)
-            print(messages[i1] == messages[i2])
-
-
-# Example usage:
-n = int(input())
-messages = []
-for _ in range(n):
-    msg, sender, receiver = input().split()
-    messages.append(Message(msg, int(sender), int(receiver)))
-
-q = int(input())
-queries = [input() for _ in range(q)]
-
-process_queries(queries, messages)
+url = 'https://parseapi.back4app.com/classes/Cepcorreios_CEP?'
+headers = {
+    'X-Parse-Application-Id': 'KDscoJcVWclrKOKMoSmmkwzVnr2NVTJleUA17X0f', # This is your app's application id
+    'X-Parse-REST-API-Key': 'qkxoXIfgK4oM3NAqdc0lFUw73wVpkHXymNdwHdoW' # This is your app's REST API key
+}
+data = json.loads(requests.get(url, headers=headers).content.decode('utf-8')) # Here you have the data that you need
+print(json.dumps(data, indent=2))

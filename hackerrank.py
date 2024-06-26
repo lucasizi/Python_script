ea6c9c49a7ab5d507f970449c4b03e52489997e9
Lucas Izidoro <lucasizi10@hotmail.com>
2024-06-26 15:25:04 -0300

    att

--- hackerrank.py
+++ hackerrank.py
@@ -1,2 +1,30 @@
-nome = 'Lucas'
-print(nome)
+#operacao bancaria
+
+texto = input("Digite o texto: ")
+vogais = "AEIOU"
+count = 0
+
+for letra in texto:
+    if not letra.upper() in vogais:
+        print(letra)
+
+print()
+
+for letra in texto:
+    if not letra.upper() in vogais:
+        count = count + 1
+    
+print(count, end="")
+print()
+
+for numero in range (0, 11):
+    print(numero, end="")
+print()
+for numero in range (0, 11,2):
+    print(numero, end="")
+print()
+for numero in range (0, 11,6):
+    print(numero, end="")
+print()
+
+
---
+++ while.py
@@ -0,0 +1,28 @@
+opcao = -1
+saldo = 5000
+limite = 0
+
+print("Olá, bem-vindo ao banco, o que deseja fazer?")
+
+while opcao !=0:
+    limite = limite +1
+    opcao = int(input(" [1] Realizar saque \n [2] Ver extrato \n [0] Sair \n Digite sua opcao: "))
+
+    if opcao == 1:
+        print("Ok, você decidiu sacar, qual o valor deseja?")
+        saque = int(input("Digite o valor: "))
+        if saque <= saldo:
+            saldo = saldo - saque
+        else:
+            print(f"Valor desejado é maior que o saldo disponivel de: {saldo}!")
+    elif opcao == 2:
+        print("Exibindo extrato... ")
+        print(f"Você possui {saldo}")
+    else:
+        print("Obrigado por usar o nosso banco!")
+
+    if limite == 5:
+        print("Você excedeu o limite diario de operações! Tente novamente amanhã! ")
+        break
+        
+exit()

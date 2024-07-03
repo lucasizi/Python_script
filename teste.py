ae23f652dc1f79426ad1da392881c2ebb475c7b5
Lucas Izidoro <lucasizi10@hotmail.com>
2024-07-03 13:51:30 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,17 +1,16 @@
-def main():
+crack = ""
+while True:
     user = str(input("Insira seu usuario: "))
     password = str(input("insira sua senha: "))
-    tamanho = len(password)
-
-    senha = "*" * tamanho
-
-    while user == password:
-        print("Usario igual a senha. Insira credenciais validas!.")
-        user = str(input("Insira seu usuario: "))
-        password = str(input("insira sua senha: "))
 
     if user != password:
-        print("Seu usuario é",user,"e sua senha é",senha)
+        
+        for i in range(len(password)):
+            crack += "*"
 
+        print("Seu usuario é",user,"e sua senha é",crack)
+    
+        break
 
-main ()
+    else:
+        print("Usario igual a senha. Insira credenciais validas!.")
